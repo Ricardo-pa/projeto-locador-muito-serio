4c37c500fea294176247e322d0e337c853f6251c
Ricardo-pa <aliandra0122@gmail.com>
2025-08-27 12:13:14 -0300

    add Registro de Moto

--- src/main/java/veiculos/moto.java
+++ src/main/java/veiculos/moto.java
@@ -8,21 +8,21 @@ package veiculos;
  *
  * @author ricardo.parreira
  */
-public class moto extends Veiculo {
+public class Moto extends Veiculo {
    private int cilindradas;
     private String tipoPartida;
-    private boolean temBau;
+    private String combustivel;
     private String tipoFreio;
-    private boolean guidonEsportivo;
+    private String guidon;
     private boolean alugado;
     
-    public moto(String nome, String Marca, String modelo, int ano, String placa, int cilindradas, String tipoPartida, boolean temBau, String tipoFreio, boolean guidonEsportivo){
+    public Moto(String nome, String Marca, String modelo, int ano, String placa, int cilindradas, String tipoPartida, String combustivel, String tipoFreio, String guidon){
         super(nome, Marca, modelo, ano, placa);
         this.cilindradas = cilindradas;
         this.tipoPartida = tipoPartida;
-        this.temBau = temBau;
+        this.combustivel = combustivel;
         this.tipoFreio = tipoFreio;
-        this.guidonEsportivo = guidonEsportivo;
+        this.guidon = guidon;
         
         
         
